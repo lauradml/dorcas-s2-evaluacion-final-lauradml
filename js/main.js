be3bd301216a5a7a0c2eae16d36487e5953dc223
lauradml <laura-domingo.82@gmail.com>
2018-07-11 16:44:31 +0200

    conseguido elegir favorito :-)

--- js/main.js
+++ js/main.js
@@ -12,40 +12,52 @@ var filmUl = document.querySelector('.films-ul');
 
 
 function listFilms(){
-filmUl.innerHTML= " ";
+  filmUl.innerHTML=' ' ;
   var film= 'http://api.tvmaze.com/search/shows?q=' +  input.value;
   fetch(film)
-  .then(function(response){
-    return response.json();
+    .then(function(response){
+      return response.json();
 
-  })
-  .then(function(json){
+    })
+    .then(function(json){
     // console.log(json[1].show);
     // console.log(json[1].show.image);
-    var series = json
-    for (var i = 0; i < series.length; i++){
-      var li = document.createElement('li');
-      // para el titulo
-      var tituloSerie= document.createElement('h2');
-      var content = document.createTextNode(series[i].show.name);
-      tituloSerie.appendChild(content);
-      li.appendChild(tituloSerie);
-      // para la imagen
-      var imageSerie= document.createElement('img');
-      if(series[i].show.image === null){
-        imageSerie.src= 'https://via.placeholder.com/210x295/cccccc/666666/?text=TV'
+    // con esta variable mi json se llama series
+      var series = json
+      for (var i = 0; i < series.length; i++){
+      // lista es todos los li que se crean
+        var lista = document.createElement('li');
+        // voy añadir clse a los li
+        lista.classList.add('caja-li');
+        // creo un evento para todos los li y la funcion  la tengo abajo
+        lista.addEventListener("click", cambiarColor);
 
-      }else{
-        imageSerie.src= series[i].show.image.medium;
+        // para el titulo
+        var tituloSerie= document.createElement('h2');
+        var content = document.createTextNode(series[i].show.name);
+        tituloSerie.appendChild(content);
+        lista.appendChild(tituloSerie);
+        // para la imagen
+        var imageSerie= document.createElement('img');
+        if(series[i].show.image === null){
+          imageSerie.src= 'https://via.placeholder.com/210x295/cccccc/666666/?text=TV'
+
+        }else{
+          imageSerie.src= series[i].show.image.medium;
+        }
+
+        lista.appendChild(imageSerie);
+        // para meter el li en el lu
+        filmUl.appendChild(lista);
+
       }
 
-      li.appendChild(imageSerie);
-      // para meter el li en el lu
-      filmUl.appendChild(li);
+    });
 
-    }
-
-  });
+// funcion que añade o quita la clase para favorito
+  function cambiarColor(event) {
+    event.currentTarget.classList.toggle('favoritos');
+  }
 }
 
 
